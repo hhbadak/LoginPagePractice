d156910d4df68335c9332da31b55ca27c9fb03af
Hasan Hüseyin BADAK <hsnhsynesk@gmail.com>
2023-03-11 00:48:01 +0300

    .
    Homework completed

---
+++ LoginPage.js
@@ -0,0 +1,11 @@
+var isSignUp = false;
+function loginClick()
+{
+    alert("Login Successful");
+}
+function signUpClick()
+{
+    prompt("User Name");
+    prompt("E-Mail");
+    prompt("Password");
+}
